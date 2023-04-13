5412b1429d9edc23743658782f492a456c9db26c
@apple-fritter <125221355+apple-fritter@users.noreply.github.com>
2023-04-13 14:34:34 -0700

    Create AppInfo.java

---
+++ src/AppInfo.java
@@ -0,0 +1,29 @@
+package com.example.mylauncher;
+
+import android.graphics.drawable.Drawable;
+
+public class AppInfo {
+
+    private String name;
+    private String packageName;
+    private Drawable icon;
+
+    public AppInfo(String name, String packageName, Drawable icon) {
+        this.name = name;
+        this.packageName = packageName;
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public Drawable getIcon() {
+        return icon;
+    }
+
+}
